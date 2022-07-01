7ebf807a945dbb44b5e512ad92ccb1f523c48e41
Seoyul Kim <zoeyul07@gmail.com>
2022-07-01 15:32:13 +0900

    chore: fontawesome 패키지 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 import "./index.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 
 ReactDom.render(
   <React.StrictMode>
