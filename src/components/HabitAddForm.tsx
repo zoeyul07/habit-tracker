5c6529ff05dbea0eeea29e755b52135c9f59c884
Seoyul Kim <zoeyul07@gmail.com>
2022-07-08 10:56:04 +0900

    chore: 코드 관련 주석 추가

--- src/components/HabitAddForm.tsx
+++ src/components/HabitAddForm.tsx
@@ -3,6 +3,9 @@ import React, { PureComponent, RefObject } from "react";
 interface Props {
   onAdd: (name: string) => void;
 }
+//pureComponent는 shallow로 state와 props를 비교해서 변경사항이 있을 경우에만 리렌더링을 해준다.
+/*그래서 object의 데이터만 변경되고 reference가 바뀌지 않을 경우에는 리렌더링이 되지 않으므로 불변성을 늘 유지하고 새로 object를 만들어서 props로 내려주도록 한다.*/
+
 class HabitAddForm extends PureComponent<Props> {
   formRef: RefObject<HTMLFormElement> = React.createRef();
   inputRef: RefObject<HTMLInputElement> = React.createRef();
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 
+//개발 환경에서는 strictMode를 사용하면 두번 실행하여 두번째시 실행시 문제가 있지 않았는지 체크한다.
 ReactDom.render(
   <React.StrictMode>
     <App />
