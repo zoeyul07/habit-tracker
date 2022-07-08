1a88aeab6bec7ec70fe2388eaf1888fb53dc0368
Seoyul Kim <zoeyul07@gmail.com>
2022-07-08 11:00:13 +0900

    refactor: pureCompnent 대신 함수형으로 memo 사용

--- src/components/HabitAddForm.tsx
+++ src/components/HabitAddForm.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent, RefObject } from "react";
+import React, { RefObject } from "react";
 
 interface Props {
   onAdd: (name: string) => void;
@@ -6,32 +6,32 @@ interface Props {
 //pureComponent는 shallow로 state와 props를 비교해서 변경사항이 있을 경우에만 리렌더링을 해준다.
 /*그래서 object의 데이터만 변경되고 reference가 바뀌지 않을 경우에는 리렌더링이 되지 않으므로 불변성을 늘 유지하고 새로 object를 만들어서 props로 내려주도록 한다.*/
 
-class HabitAddForm extends PureComponent<Props> {
-  formRef: RefObject<HTMLFormElement> = React.createRef();
-  inputRef: RefObject<HTMLInputElement> = React.createRef();
+//함수의 경우 pureComponent대신 memo를 사용한다.
+const HabitAddForm = React.memo((props: Props) => {
+  const formRef: RefObject<HTMLFormElement> = React.createRef();
+  const inputRef: RefObject<HTMLInputElement> = React.createRef();
 
-  onSubmit = (event: any) => {
+  const onSubmit = (event: any) => {
     event.preventDefault();
-    const name = this.inputRef?.current?.value;
-    name && this.props.onAdd(name);
+    const name = inputRef?.current?.value;
+    name && props.onAdd(name);
     //this.inputRef?.current?.value = "";
-    if (this.formRef?.current) {
-      this.formRef.current.reset();
+    if (formRef?.current) {
+      formRef.current.reset();
     }
   };
-  render() {
-    return (
-      <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-        <input
-          ref={this.inputRef}
-          type="text"
-          className="add-input"
-          placeholder="habit"
-        ></input>
-        <button className="add-button">Add</button>
-      </form>
-    );
-  }
-}
+
+  return (
+    <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        className="add-input"
+        placeholder="habit"
+      ></input>
+      <button className="add-button">Add</button>
+    </form>
+  );
+});
 
 export default HabitAddForm;
